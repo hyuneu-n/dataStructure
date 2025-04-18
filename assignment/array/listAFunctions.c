f94f6120d75e74e5831459ed8ccd72054a372421
JJERY <hyuneun@skuniv.ac.kr>
2025-04-18 15:40:22 +0900

    Array List

--- assignment/array/listAFunctions.c
+++ assignment/array/listAFunctions.c
@@ -2,59 +2,52 @@
 #include <stdio.h> // 표준 입출력 함수 사용을 위해 포함  
 #include <stdbool.h> // bool 타입 사용을 위해 포함  
 
-void Init(listType *Lptr) {  
+void Init(listType *Lptr) { // Lptr이 가리키고 있는 리스트를 초기화 시킴
     Lptr->Count = 0; // 리스트의 카운트를 0으로 초기화  
 }  
 
 bool IsEmpty(listType *Lptr) {  
-    return (Lptr->Count == 0); // 리스트가 비어있으면 true 반환  
+    return (Lptr->Count == 0);  // Lptr이 가리키고 있는 리스트가 비어있으면 ‘참‘을 '비어있지 않으면 ’거짓‘을 반환
 }  
 
 int Length(listType *Lptr) {  
     return Lptr->Count; // Lptr이 가리키고 있는 리스트에 들어 있는 의미있는 값의 개수를 반환
 }  
 
-void Insert(listType *Lptr, int Position, int Item) {  
-    if (Lptr == NULL) // 리스트 포인터가 NULL일 경우  
-        printf("List pointer is NULL\n"); // 경고 메시지 출력  
-    else if (Lptr->Count == MAX) // 리스트가 가득 찼을 경우  
+//Item이라는 data를 Position번째에 삽입
+void Insert(listType *Lptr, int Position, int Item) { 
+    if (Lptr->Count == MAX) // 리스트가 가득 찼을 경우  
         printf("List Full\n"); // 경고 메시지 출력  
-    else if ((Position > (Lptr->Count + 1)) || Position < 1) // 삽입 위치가 유효하지 않을 경우  
-        printf("Position out of Range\n"); // 경고 메시지 출력  
-    else {  
-        for (int i = Lptr->Count - 1; i >= Position - 1; i--)  
-            Lptr->Data[i + 1] = Lptr->Data[i]; // 데이터를 오른쪽으로 한 칸씩 이동  
-        Lptr->Data[Position - 1] = Item; // 원하는 위치에 값 삽입  
-        Lptr->Count++; // 데이터 개수 증가  
-        printf("Insert Completed\n"); // 완료 메시지 출력  
+    else if ((Position > (Lptr->Count + 1)) || Position < 1) //Position 범위가 올바르지 않을 때
+        printf("Position out of Range\n"); // 경고 메시지 출력 
+    else { 
+        //Position번째의 공간을 비우는 작업
+        for (int i = Lptr->Count - 1; i >= Position - 1; i--)// 맨 뒤에서부터 Position번째까지
+            Lptr->Data[i + 1] = Lptr->Data[i]; // 한칸씩 이동
+        Lptr->Data[Position - 1] = Item; //Position번째에 Item 값을 저장
+        Lptr->Count++; // 리스트의 개수 하나 증가
     }  
-}  
+}
 
-void Delete(listType *Lptr, int Position) {  
-    if (Lptr == NULL) // 리스트 포인터가 NULL일 경우  
-        printf("List pointer is NULL\n"); // 경고 메시지 출력  
-    else if (Lptr->Count == 0) // 리스트가 비어있을 경우  
-        printf("List is Empty\n"); // 경고 메시지 출력  
-    else if ((Position > Lptr->Count) || Position < 1) // 삭제 위치가 유효하지 않을 경우  
+//Position번째의 data를 삭제
+void Delete(listType *Lptr, int Position) {
+    if (Lptr->Count == 0)  //삭제할 값이 없는 경우 경고 메시지 출력
+        printf("List is Empty\n");
+    else if ((Position > Lptr->Count) || Position < 1) //Position 범위가 올바르지 않을 때
         printf("Position out of Range\n"); // 경고 메시지 출력  
-    else {  
-        for (int i = Position - 1; i < Lptr->Count - 1; i++)  
-            Lptr->Data[i] = Lptr->Data[i + 1]; // 데이터를 왼쪽으로 한 칸씩 이동  
-        Lptr->Data[Lptr->Count - 1] = 0; // 마지막 쓰레기 값 제거  
-        Lptr->Count--; // 데이터 개수 감소  
-        printf("Delete Completed\n"); // 완료 메시지 출력  
-    }  
-}  
+    else {
+        for (int i = Position - 1; i < Lptr->Count - 1; i++) //삭제 위치 다음부터 끝까지 반복
+            Lptr->Data[i] = Lptr->Data[i + 1];//한칸씩 앞으로 보냄
+        Lptr->Count--; //리스트의 개수를 하나 감소
+    }
+}
 
 void Retrieve(listType *Lptr, int Position, int *Itemptr) {  
-    if (Lptr == NULL) // 리스트 포인터가 NULL일 경우  
-        printf("List pointer is NULL\n"); // 경고 메시지 출력  
-    else if (Lptr->Count == 0) // 리스트가 비어있는 경우  
+    if (Lptr->Count == 0) // 리스트가 비어있는 경우  
         printf("List is Empty\n"); // 경고 메시지 출력  
-    else if ((Position > Lptr->Count) || Position < 1) // 위치가 유효하지 않을 경우  
+    else if ((Position > Lptr->Count) || Position < 1) //Position범위가 올바르지 않을때
         printf("Position out of Range\n"); // 경고 메시지 출력  
-    else {  
-        *Itemptr = Lptr->Data[Position - 1]; // 해당 위치의 데이터를 포인터에 저장  
-        printf("Retrieved Item: %d\n", *Itemptr); // 값을 출력  
+    else {
+        *Itemptr = Lptr->Data[Position - 1]; //Position번째의 data를 Itemptr이 가리킴
     }  
 } 
--- assignment/array/listAMain.c
+++ assignment/array/listAMain.c
@@ -1,20 +1,34 @@
-#include "listA.h"
+#include "listA.h" //리스트를 구현한 인터페이스 파일 포함
+#include <stdio.h>
 
-int main(){
-    listType L;
-    Init(&L);
-    for(int i=1;i<=10;i++)
-        Insert(&L, i, i);
-    for(int i=1;i<=10;i++)
-        Retrieve(&L, i, &L);
-    Length(&L);
-    for(int i=1;i<10;i++){
-        if(i%3==0)
-            Delete(&L, i);
-        else
-            return 0;
+int main() {
+    listType L; // 리스트 L 생성
+    Init(&L); // 리스트 초기화
+
+    // 1부터 10까지 Insert
+    for (int i = 1; i <= 10; i++) {
+        Insert(&L, i, i); // i번째 위치에 i값 삽입
     }
-    for(int i=1;i<=10;i++)
-        Retrieve(&L, i, &L);
-    Length(&L);
+
+    // 리스트 내용 출력
+    printf("=== 리스트 전체 출력 (삽입 완료 후) ===\n");
+    int len = Length(&L); // 현재 리스트 길이 구함
+    int result;
+    for (int i = 1; i <= len; i++) {
+        Retrieve(&L, i, &result); // i번째 값 출력
+    }
+
+    // 3, 6, 9번째 원소 삭제
+    Delete(&L, 9); //먼저 9번째 삭제 (뒤부터 삭제해야 인덱스 밀림 없음)
+    Delete(&L, 6);
+    Delete(&L, 3);
+
+    // 삭제 후 리스트 출력
+    printf("\n=== 리스트 전체 출력 (삭제 후) ===\n");
+    len = Length(&L); // 삭제 이후 리스트 길이 갱신
+    for (int i = 1; i <= len; i++) {
+        Retrieve(&L, i, &result); // i번째 값 출력
+    }
+
+    return 0;
 }
